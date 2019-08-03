aa459d1a94204c114330f886a3af9ef460f3d15d
michealcoleyoung <coleyoungmusic@gmail.com>
2019-08-03 10:59:15 -0500

    added templates and static folder

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 
 import requests
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 @app.route('/')
-def hello_world():
-    return 'Hello World'
+def index():
+    return render_template('weather.html')
