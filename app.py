9df0b91b764f6fa64248e01c50fbd61e6258e99e
michealcoleyoung <coleyoungmusic@gmail.com>
2019-08-06 21:29:16 -0500

    updated url in app.py

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9505be1fdbed303d342537db528a8c23'
+    city = 'Dallas'
+
+    r = requests.get(url.format(city)).json()
+    print(r)
+
+
     return render_template('weather.html')
 
 
