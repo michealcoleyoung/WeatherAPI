fed588d84c9c8732165fc22d45b7e7a46d07955b
michealcoleyoung <coleyoungmusic@gmail.com>
2019-08-14 21:46:46 -0500

    updated app to include multiple cities and added database

--- app.py
+++ app.py
@@ -5,24 +5,38 @@ from flask_sqlalchemy import SQLAlchemy
 import pprint
 
 app = Flask(__name__)
-app.config['DEBUG'] = False
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
+
+db = SQLAlchemy(app)
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def index():
-    url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9505be1fdbed303d342537db528a8c23'
-    city = 'Dallas'
+    cities = City.query.all()
 
-    r = requests.get(url.format(city)).json()
-    pprint.pprint(r)
+    weather_data = []
 
-    weather = {
-        'name': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon']
-    }
+    for city in cities:
 
-    pprint.pprint(weather)
-    return render_template('weather.html', weather=weather)
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=9505be1fdbed303d342537db528a8c23'
+        r = requests.get(url.format(city.name)).json()
+
+        print(r)
+
+        weather = {
+            'name': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon']
+        }
+
+        weather_data.append(weather)
+
+    # pprint.pprint(weather)
+    return render_template('weather.html', weather_data=weather_data)
 
 
